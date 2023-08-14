f16034df50fbaf695487a686a96195d6b6593508
DESKTOP-JO3QE3M\82108 <leejin914418@gmail.com>
2023-08-15 00:03:55 +0900

    6.Effects

---
+++ my_react_app/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import Button from "./Button";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once");
+  };
+
+  useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("I run when keyword changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when counter changes");
+  }, [counter]);
+
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me!</button>
+    </div>
+  );
+}
+
+export default App;
