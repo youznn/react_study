ff463e24acd8b5a5aa07f933edc1cd3140f44946
DESKTOP-JO3QE3M\82108 <leejin914418@gmail.com>
2023-08-15 00:19:42 +0900

    6.+ cleanup function

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -1,35 +1,21 @@
 import { useState, useEffect } from "react";
 import Button from "./Button";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("distroyed :(");
+  }, []);
+  return <h1>Hello~</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("I run only once");
-  };
-
-  useEffect(iRunOnlyOnce, []);
-  useEffect(() => {
-    console.log("I run when keyword changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when counter changes");
-  }, [counter]);
-
+  const [showing, setshowing] = useState(false);
+  const onClick = () => setshowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}> {showing ? "hide!" : "show!"} </button>
     </div>
   );
 }
